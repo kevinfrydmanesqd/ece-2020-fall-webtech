88b3c0adf6c90a30ac981a01c0ae6d4db30e1c77
David Worms <david@adaltas.com>
2020-09-25 08:57:51 +0200

    storage: replace message uuid with time

--- courses/webtech/modules/03.storage/lab/test/messages.js
+++ courses/webtech/modules/03.storage/lab/test/messages.js
@@ -10,52 +10,52 @@ describe('messages', () => {
   
   it.skip('list empty', async () => {
     // Create a channel
-    await supertest(app)
+    const {body: channel} = await supertest(app)
     .post('/channel')
     .send({name: 'channel 1'})
     // Get messages
     const {body: messages} = await supertest(app)
-    .get('/channel/1/messages')
+    .get(`/channel/${channel.id}/messages`)
     .expect(200)
     messages.should.match([])
   })
   
   it.skip('list one message', async () => {
     // Create a channel
-    await supertest(app)
+    const {body: channel} = await supertest(app)
     .post('/channel')
     .send({name: 'channel 1'})
     // and a message inside it
     await supertest(app)
-    .post('/channel/1/messages')
+    .post(`/channel/${channel.id}/messages`)
     .send({content: 'Hello ECE'})
     // Get messages
     const {body: messages} = await supertest(app)
-    .get('/channel/1/messages')
+    .get(`/channel/${channel.id}/messages`)
     .expect(200)
     messages.should.match([{
-      id: /^\w+-\w+-\w+-\w+-\w+$/,
+      creation: /^d+$/
       content: 'Hello ECE'
     }])
   })
   
   it.skip('add one element', async () => {
     // Create a channel
-    await supertest(app)
+    const {body: channel} = await supertest(app)
     .post('/channel')
     .send({name: 'channel 1'})
     // Create a message inside it
     const {body: message} = await supertest(app)
-    .post('/channel/1/messages')
+    .post(`/channel/${channel.id}/messages`)
     .send({content: 'Hello ECE'})
     .expect(201)
     message.should.match({
-      id: /^\w+-\w+-\w+-\w+-\w+$/,
+      creation: /^d+$/
       content: 'Hello ECE'
     })
     // Check it was correctly inserted
     const {body: messages} = await supertest(app)
-    .get('/channel/1/messages')
+    .get(`/channel/${channel.id}/messages`)
     messages.length.should.eql(1)
   })
   
